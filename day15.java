d6a36f33351bc1783d1d212a67c0299e2b8d32f4
neuraxinc001-cell <neuraxinc001@gmail.com>
2026-09-16 23:43:29 +0800

    Create day15.java

---
+++ day15.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class day14 {
+
+    public static void main(String[] args) {
+        Scanner i = new Scanner (System.in);
+
+        System.out.print("Angka pertama : ");
+        int a = i.nextInt();
+
+        System.out.print("Angka kedua : ");
+        int b = i.nextInt();
+
+        System.out.printf("Untuk Perkalian (*) hasil nya : %d", a*b);
+        System.out.printf("%nUntuk Pembagian (/) hasil nya : %d", a/b);
+    }
+}
